b0ee826ba9be8922539d9c832bd648fc0d9395f7
Luke8563 <122496000+Luke8563@users.noreply.github.com>
2023-01-29 23:34:24 -0800

    FixedSearchBar

--- NumberServer.java
+++ NumberServer.java
@@ -6,23 +6,26 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-    ArrayList<String> list = new ArrayList<String>();
+    ArrayList<String> arr = new ArrayList<String>();
+    
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Luke's List:");
-
+            return String.format("Luke's Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
-
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                    arr.add(parameters[1]);
+                    String list = "";
+                    for(int i = 0; i < arr.size(); i++){
+                        list += arr.get(i) + "\n";
+                    }
+                    return list;
                 }
             }
             return "404 Not Found!";
@@ -30,7 +33,7 @@ class Handler implements URLHandler {
     }
 }
 
-class SearchEngine {
+class NumberServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
